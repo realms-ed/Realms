080515a87a92c20cd48a1bdec6d79e8777c1eb32
raylennon <48305386+raylennon@users.noreply.github.com>
2022-01-01 13:59:29 -0500

    zoom api working?

--- public/js/module.js
+++ public/js/module.js
@@ -1,12 +1,50 @@
-export function joinFunc() {
+const client = ZoomMtgEmbedded.createClient();
+
+let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+client.init({
+  debug: true,
+  zoomAppRoot: meetingSDKElement,
+  language: 'en-US',
+  customize: {
+    meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+    toolbar: {
+      buttons: [
+        {
+          text: 'Custom Button',
+          className: 'CustomButton',
+          onClick: () => {
+            console.log('custom button');
+          }
+        }
+      ]
+    }
+  }
+});
+
+export function joinFunc() {    
+
     var xhr = new XMLHttpRequest();
-    var url = "https://localhost:4000/";
+    var url = "http://localhost:4000/";
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
-            console.log(json);;
+            console.log(json.signature);
+            client.join({
+                signature: json.signature,
+                meetingNumber: 123456789,
+                userName: "Ray",
+                apiKey: "NRF1ny_0Sz2VWOlYb7szcg",
+                passWord: "",
+                success: (success) => {
+                  console.log(success)
+                },
+                error: (error) => {
+                  console.log(error)
+                }
+              })
         }
     };
     var data = JSON.stringify({
