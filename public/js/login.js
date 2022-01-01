5c57465b9aacc9b0802eda4d03bf9b46cf0c597c
raylennon <48305386+raylennon@users.noreply.github.com>
2022-01-01 15:01:05 -0500

    OAuth First Swing

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,4 @@
-var auth_url = "https://zoom.us/oauth/authorize?response_type=code&client_id=JomeAb3LScWahNG6mYWh3A&redirect_uri=https://realms-ed.herokuapp.com"
+var auth_url = "https://zoom.us/oauth/authorize?response_type=code&client_id=JomeAb3LScWahNG6mYWh3A&redirect_uri=https://realms-ed.herokuapp.com/redirect"
 
 export function loginFunc() {
     window.location.href = auth_url
