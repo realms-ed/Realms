5d23dcbb5efd8852da39f1e684afe191a8767c8f
raylennon <48305386+raylennon@users.noreply.github.com>
2022-01-22 18:27:40 -0500

    http to https

--- index.js
+++ index.js
@@ -114,9 +114,9 @@ var SamlStrategy = require('passport-saml').Strategy;
 var strategy = new SamlStrategy(
   {
     path : '/login/callback',
-    callbackUrl : 'http://realms-ed.herokuapp.com/login/callback',
+    callbackUrl : 'https://realms-ed.herokuapp.com/login/callback',
     entryPoint : 'https://shib.oit.duke.edu/idp/profile/SAML2/Redirect/SSO',
-    issuer : 'http://realms-ed.herokuapp.com',
+    issuer : 'https://realms-ed.herokuapp.com',
     cert : cert
   },
   function(profile, done) {
